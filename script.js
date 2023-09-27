ea0582bdbcda65c6b261753fae3188d7055c314c
Jose G <melespinet@Mels-MacBook-Air.local>
2023-09-27 19:22:20 -0400

    Styles and logic progress

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('It works')
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('myForm');
+    const result = document.getElementById('result');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const tittleInput = document.getElementById('tittle');
+        const ratingInput = document.getElementById('rating');
+
+        const tittle = tittleInput.value;
+        const rating = ratingInput.value;
+
+        result.innerHTML = `<p>Tittle: ${tittle}</p><p>Rating: ${rating}</p>`;
+    });
+});
