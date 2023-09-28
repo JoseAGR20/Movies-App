d46e9e1a9d56657578566e94555fea64b17f6a2e
Jose G <melespinet@Mels-MacBook-Air.local>
2023-09-27 20:30:52 -0400

    Requirements completed

--- script.js
+++ script.js
@@ -9,8 +9,30 @@ document.addEventListener('DOMContentLoaded', function () {
         const ratingInput = document.getElementById('rating');
 
         const tittle = tittleInput.value;
-        const rating = ratingInput.value;
+        const rating = parseInt(ratingInput.value);
 
-        result.innerHTML = `<p>Tittle: ${tittle}</p><p>Rating: ${rating}</p>`;
+        if (tittle.length < 2) {
+            alert('Tittle must have at least 2 character!')
+            return;
+        }
+
+        if (isNaN(rating) || rating < 1 || rating > 10) {
+            alert('Please enter a rating between 1 and 10.');
+            return;
+        }
+
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `<p>Tittle: ${tittle}</p><p>Rating: ${rating}</p><button class="remove-button">Remove</button>`;
+
+        result.appendChild(newDiv);
+
+        tittleInput.value = '';
+        ratingInput.value = '';
+
+        const removeButton = newDiv.querySelector('.remove-button');
+        removeButton.addEventListener('click', function() {
+            result.removeChild(newDiv);
+        });
+
     });
 });
